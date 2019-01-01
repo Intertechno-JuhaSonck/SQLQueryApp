2612f788946bd28517c966671b26bea77d4ef30a
Juha Sonck <juha.sonck@intertechno.org>
2019-01-01 18:20:44 +0200

    Kehitetty konsolisovellus, joka hakee minkä tahansa SQL-lauseen perusteella datan NorthWind-tietokannasta ja tulostaa sen konsolille taulukoituna

---
+++ SQLQueryApp/OmiaFunktioita.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SQLQueryApp
+{
+    public class OmiaFunktioita
+    {
+        public static void SuoritaSQLLauseTaulukkoon(SqlConnection conn, string SQLLause)
+        {
+            string sarakeNimi = "", sarakeArvo = "";
+            int i = 0, j = 0, k = 0;
+            SqlCommand cmd = new SqlCommand(SQLLause, conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            DataTable schemaTable = reader.GetSchemaTable();   //using System.Data;
+
+
+            foreach (DataRow rivi in schemaTable.Rows)
+            {
+                foreach (DataColumn column in schemaTable.Columns)
+                {
+                    if (column.ColumnName == "ColumnName")
+                    {
+                        j++;
+                        sarakeNimi = rivi[column].ToString();
+                        sarakeNimi = (sarakeNimi.PadRight(15).Substring(0, 15)) + " ";
+                        Console.Write(sarakeNimi);
+                    }
+
+                }
+            }
+            Console.WriteLine();
+            while (reader.Read())
+            {
+                i++;
+                for (k = 0; k < j; k++)
+                {
+                    //sarakeArvo = reader.GetString(k).ToString();
+                    sarakeArvo = reader.GetValue(k).ToString();
+                    sarakeArvo = (sarakeArvo.PadRight(15).Substring(0, 15)) + " ";
+                    if (k < j - 1)
+                    {
+                        Console.Write(sarakeArvo);
+                    }
+                    else Console.WriteLine(sarakeArvo);
+                }
+            }
+            Console.WriteLine("Yhteensä " + i.ToString() + " riviä ");
+            // resurssien vapautus
+            reader.Close();
+            cmd.Dispose();
+        }
+    }
+}
--- SQLQueryApp/Program.cs
+++ SQLQueryApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,55 @@ namespace SQLQueryApp
     {
         static void Main(string[] args)
         {
+            SqlConnection TkYhteys;
+            string SQL = "";
+            TkYhteys = AvaaTietokanta();
+            SQL = KysySQLLause();
+            Console.WriteLine(SQL);
+            //SuoritaSQLLauseb(TkYhteys, SQL);
+            OmiaFunktioita.SuoritaSQLLauseTaulukkoon(TkYhteys, SQL);
+            Console.ReadLine();
+            TkYhteys.Dispose();
+
         }
+
+        private static SqlConnection AvaaTietokanta()
+        {
+            //using System.Data.SqlClient;
+            string connStr = "Server=L17HE01A\\SQLEXPRESS;Database=Northwind;Trusted_Connection=True;";
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+            return conn;
+        }
+
+        private static string KysySQLLause()
+        {
+            string SQL = "";
+            Console.Write("Anna SQL-lause: ");
+            SQL = Console.ReadLine();
+            return SQL;
+        }
+
+        private static void SuoritaSQLLause(SqlConnection conn, string SQLLause)
+        {
+            int i = 0;
+            SqlCommand cmd = new SqlCommand(SQLLause, conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                i++;
+                string kentta0 = reader.GetValue(0).ToString();
+                string kentta1 = reader.GetString(1);
+                string kentta2 = reader.GetString(2);
+                Console.WriteLine("Löytyi rivi: " + kentta0 + " " + kentta1 + " " + kentta2);
+            }
+            Console.WriteLine("Yhteensä " + i.ToString() + " riviä ");
+
+            // resurssien vapautus
+            reader.Close();
+            cmd.Dispose();
+        }
+
     }
 }
